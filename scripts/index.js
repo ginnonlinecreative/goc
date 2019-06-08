82de2babc46a158433afdafff4ba32214da783ba
unknown <nathanginnlimited@gmail.com>
2019-06-08 08:55:49 +0100

    menu

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,6 @@
 
 function welcomeLookUp(val) {
 
-
-  // Only change code below this line
   var lookup =  {
  "about": '{"name":"Creative Problem solver for all types of business.Based in the Essex I am always willing to assist business with online issues from the surrounding areas at a discounted cost.However if you are from a bit further lets chat online and see how best I can help", "color":"#663399", "background":"#e0ebe8"}',
 "how":  '{"name":"After our initial meeting online or face to face I will come up with a mockup and of your solution for you to view online. This wont be the finished solution but will give you an idead of what im sthinking", "color":"red", "background":"green"}',
@@ -19,3 +17,7 @@ function welcomeLookUp(val) {
   document.getElementById("wrapper").style.background = result.color;
   document.getElementById("aboutTxt").textContent = result.name;
 }
+//MENU
+function classToggle() {  const navs = document.querySelectorAll('.Navbar__Items')
+navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));}
+document.querySelector('.Navbar__Link-toggle')  .addEventListener('click', classToggle);
